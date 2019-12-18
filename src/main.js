98500dd628a08f38ed2479be1f8b5b4a29ca53e7
zoujiamin <1159826245@qq.com>
2019-12-18 17:01:19 +0800

    新增登录和注册内容

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
+import 'iview/dist/styles/iview.css' // 导入样式
 
 Vue.config.productionTip = false
 
